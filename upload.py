a406e976ff05f63588d64216d0a1e03dbbedf384
steven.patz <steven.patz@gmail.com>
2015-07-19 22:14:29 -0400

    Check quota to see if we can upload.  If we can, then check file size to see if specific file can be uploaded.

--- upload.py
+++ upload.py
@@ -5,5 +5,15 @@ from sys import argv
 script, auth = argv
 r = requests.get("https://api.vimeo.com/me", headers={"Authorization": "bearer %s" % auth } )
 parsed_json = json.loads(r.text)
-print(parsed_json['upload_quota']['space']['free'])
 
+
+free_space = (parsed_json['upload_quota']['space']['free'])
+file_size = 738737362612182376
+if free_space == 0:
+  print "Can't upload, quota has been exceeded."
+else:
+  print "Uploading is fine, now we need to determine if file size exceeds remaining free space"
+  if file_size < free_space:
+    print "Can start the uploading porcess as we have enough space left."
+  else:
+    print "Not enough quota for this file, try another file perhaps?"
